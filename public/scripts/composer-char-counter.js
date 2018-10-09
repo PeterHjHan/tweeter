0dc66581df6644c1f43b8832b78e945620b4bc25
= <peterhjhan@gmail.com>
2018-10-09 23:45:10 +0000

    adding -1 to the counter failed, reverting back

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
   
   $('.tweet-text-box').on('keyup', function () {
-    const tweetInputLength = this.value.length;
     const defaultCounter = $(this).parents("form").children('span.counter');
-
     $(defaultCounter).text(defaultCounter.text() -1);
+      if(defaultCounter < 0) {
+        $(defaultCounter.text()).css({"color": "red"});
+      }
 
   });
 });
