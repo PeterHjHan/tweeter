9a989a6388149b9aed7d1eb9e591add870777e27
= <peterhjhan@gmail.com>
2018-10-09 23:42:15 +0000

    span.counter now reflects to reduce the number by one

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,22 +2,9 @@ $(document).ready(function () {
   
   $('.tweet-text-box').on('keyup', function () {
     const tweetInputLength = this.value.length;
-    // const defaultCounter = document.querySelector('span.counter').textContent;
-    const defaultCounter = $(this).parents("form").children('span.counter').text();
- 
+    const defaultCounter = $(this).parents("form").children('span.counter');
 
-    console.log( defaultCounter - tweetInputLength);
-    
-
-
-  
-  
-    
-    
-
-    
-    ;
-
+    $(defaultCounter).text(defaultCounter.text() -1);
 
   });
 });
