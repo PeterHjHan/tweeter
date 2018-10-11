991b26bc4703737c2d5a2562705bcd9df096a41a
= <peterhjhan@gmail.com>
2018-10-11 23:14:11 +0000

    revert back to original file

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,12 +6,6 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   
-  db.collection("tweets").find().toArray((err, result) => {
-    // Lazy error handling:
-    if (err) throw err;
-
-    
-
   return {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
@@ -30,5 +24,4 @@ module.exports = function makeDataHelpers(db) {
     }
 
   };
-});
 }
